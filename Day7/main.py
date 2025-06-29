d22d3b75e64ee0aa1574e4c40b00423f3cb7b97d
Luigi Lasso <llasso05@gmail.com>
2025-06-29 15:21:33 -0500

    Completed day 7 adding ascii art and live count.

--- Day7/main.py
+++ Day7/main.py
@@ -1,19 +1,131 @@
 import random
 
-word_list = ["aardvark", "baboon", "camel"]
+hangman_art = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. then print it
+
+word_list = ["aardvark", "baboon", "camel"]
+word_iterator = 0
+
+# Lives
+lives = 7
+
+# Randomly choose a word from the word_list and assign it to a variable called chosen_word. then print it
 random_number = random.randint(0, 2)
 chosen_word = word_list[random_number]
-print(chosen_word)
-
-# TODO-2 - Ask user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: \n").lower()
+# print(chosen_word)
 
 
-# TODO-3 - Check if the letteer the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it.
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+# Create a "Placeholder" with the same number of blanks as the chosen word
+placeholder = len(chosen_word) * "_"
+print(placeholder)
+
+while placeholder != chosen_word:
+
+    attempt = ""
+    # Ask user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+    guess = input("Guess a letter: \n").lower()
+
+    # Create a "display that puts the guess letter in the right
+
+    for letter1, letter2 in zip(chosen_word, placeholder):
+        if letter1 == guess:
+            attempt += guess
+        else:
+            attempt += letter2
+
+        placeholder = attempt
+
+    if guess not in attempt:
+        lives = lives - 1
+        print(hangman_art[word_iterator])
+        word_iterator = word_iterator + 1
+
+
+    if lives == 0:
+        print("""
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⡀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⠀⠀⠀⢀⣴⣿⡶⠀⣾⣿⣿⡿⠟⠛⠁
+⠀⠀⠀⠀⠀⠀⣀⣀⣄⣀⠀⠀⠀⠀⣶⣶⣦⠀⠀⠀⠀⣼⣿⣿⡇⠀⣠⣿⣿⣿⠇⣸⣿⣿⣧⣤⠀⠀⠀
+⠀⠀⢀⣴⣾⣿⡿⠿⠿⠿⠇⠀⠀⣸⣿⣿⣿⡆⠀⠀⢰⣿⣿⣿⣷⣼⣿⣿⣿⡿⢀⣿⣿⡿⠟⠛⠁⠀⠀
+⠀⣴⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⢠⣿⣿⣹⣿⣿⣿⣿⣿⣿⡏⢻⣿⣿⢿⣿⣿⠃⣼⣿⣯⣤⣴⣶⣿⡤⠀
+⣼⣿⠏⠀⣀⣠⣤⣶⣾⣷⠄⣰⣿⣿⡿⠿⠻⣿⣯⣸⣿⡿⠀⠀⠀⠁⣾⣿⡏⢠⣿⣿⠿⠛⠋⠉⠀⠀⠀
+⣿⣿⠲⢿⣿⣿⣿⣿⡿⠋⢰⣿⣿⠋⠀⠀⠀⢻⣿⣿⣿⠇⠀⠀⠀⠀⠙⠛⠀⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀
+⠹⢿⣷⣶⣿⣿⠿⠋⠀⠀⠈⠙⠃⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠈⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣴⣶⣦⣤⡀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⣠⡇⢰⣶⣶⣾⡿⠷⣿⣿⣿⡟⠛⣉⣿⣿⣿⠆
+⠀⠀⠀⠀⠀⠀⢀⣤⣶⣿⣿⡎⣿⣿⣦⠀⠀⠀⢀⣤⣾⠟⢀⣿⣿⡟⣁⠀⠀⣸⣿⣿⣤⣾⣿⡿⠛⠁⠀
+⠀⠀⠀⠀⣠⣾⣿⡿⠛⠉⢿⣦⠘⣿⣿⡆⠀⢠⣾⣿⠋⠀⣼⣿⣿⣿⠿⠷⢠⣿⣿⣿⠿⢻⣿⣧⠀⠀⠀
+⠀⠀⠀⣴⣿⣿⠋⠀⠀⠀⢸⣿⣇⢹⣿⣷⣰⣿⣿⠃⠀⢠⣿⣿⢃⣀⣤⣤⣾⣿⡟⠀⠀⠀⢻⣿⣆⠀⠀
+⠀⠀⠀⣿⣿⡇⠀⠀⢀⣴⣿⣿⡟⠀⣿⣿⣿⣿⠃⠀⠀⣾⣿⣿⡿⠿⠛⢛⣿⡟⠀⠀⠀⠀⠀⠻⠿⠀⠀
+⠀⠀⠀⠹⣿⣿⣶⣾⣿⣿⣿⠟⠁⠀⠸⢿⣿⠇⠀⠀⠀⠛⠛⠁⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+""")
+        break
+
+    print(placeholder + "\n")
+    print(f"You have {lives} lives left!")
+
+    if placeholder == chosen_word:
+        print("""
+                    88                                                       
+            ""              ,d                                       
+                            88                                       
+8b       d8 88  ,adPPYba, MM88MMM ,adPPYba,  8b,dPPYba, 8b       d8  
+`8b     d8' 88 a8"     ""   88   a8"     "8a 88P'   "Y8 `8b     d8'  
+ `8b   d8'  88 8b           88   8b       d8 88          `8b   d8'   
+  `8b,d8'   88 "8a,   ,aa   88,  "8a,   ,a8" 88           `8b,d8'    
+    "8"     88  `"Ybbd8"'   "Y888 `"YbbdP"'  88             Y88'     
+                                                            d8'      
+                                                           d8'       
+                                                           """)
+
+
+
