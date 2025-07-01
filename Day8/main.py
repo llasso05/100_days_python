c054ef2ee95b185c357b4172a9fad5a9564a6b4a
LuigiLasso <llasso05@gmail.com>
2025-07-01 16:47:05 -0500

    updated todo list in cyphers-decypher.

---
+++ Day8/main.py
@@ -0,0 +1,13 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number: \n"))
+
+# TODO-1 Create a function called encrypt that takes original text and shift amount as 2 inputs
+# TODO-2 Inside the encrypt function shift each letter of the original_text forwards in the alphabet by the shift amount
+#  and print the encrypted letter
+# TODO-4 What happens if you tro to shiftz forwards by 9?
+# TODO-3 Call the encrypt() function and pass in the user. You should be able to test the code and encrypt a message to
+# the user.
