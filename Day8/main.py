a6c440f19f8819ef4a9812160b8401bee6b6698b
LuigiLasso <llasso05@gmail.com>
2025-07-02 13:07:22 -0500

    Created new encrypted function.

--- Day8/main.py
+++ Day8/main.py
@@ -1,3 +1,4 @@
+# 26 letters
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
@@ -5,9 +6,28 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number: \n"))
 
-# TODO-1 Create a function called encrypt that takes original text and shift amount as 2 inputs
-# TODO-2 Inside the encrypt function shift each letter of the original_text forwards in the alphabet by the shift amount
-#  and print the encrypted letter
-# TODO-4 What happens if you tro to shiftz forwards by 9?
-# TODO-3 Call the encrypt() function and pass in the user. You should be able to test the code and encrypt a message to
-# the user.
+
+def encrypt():
+    encrypted_index = []
+    # iterating original message
+    for letter in text:
+        # adding shift to the indexes
+        if letter in alphabet:
+            letter_index = alphabet.index(letter) + shift
+            # calculating index over z
+            if letter_index > 25:
+                new_letter_index = letter_index - 26
+                encrypted_index.append(new_letter_index)
+            # setting new encrypted index
+            elif letter_index < 25:
+                encrypted_index.append(letter_index)
+    print(encrypted_index)
+    encrypted_message = ""
+    # encrypting message
+    for i in encrypted_index:
+        encrypted_message += alphabet[i]
+    print(encrypted_message)
+
+
+encrypt()
+
