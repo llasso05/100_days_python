73d1590b7e23076f3ebb52872a2a9ccac13b7698
Luigi Lasso <llasso05@gmail.com>
2025-07-06 15:37:51 -0500

    Completed day 8 adding decrypt without caesar function user experience

--- Day8/main.py
+++ Day8/main.py
@@ -1,36 +1,35 @@
+print("""
+           88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88        
+""")
 # 26 letters
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', " "]
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number: \n"))
+direction = ""
+quit = ""
 
 
 def encrypt(original_text, shift_amount):
-    encrypted_index = []
+    encrypted_message = ""
     # iterating original message
     for letter in original_text:
         # adding shift to the indexes
         if letter in alphabet:
             letter_index = alphabet.index(letter) + shift_amount
-            # calculating index over z
-            if letter_index > 26:
-                new_letter_index = letter_index - 27
-                encrypted_index.append(new_letter_index)
-            # setting new encrypted index
-            elif letter_index < 26:
-                encrypted_index.append(letter_index)
-    print(encrypted_index)
-    encrypted_message = ""
-    # encrypting message
-    for i in encrypted_index:
-        encrypted_message += alphabet[i]
+            letter_index %= len(alphabet)
+            encrypted_message += alphabet[letter_index]
     print(f"here is the encrypted message {encrypted_message}")
 
 
 # encrypt(text, shift)
-# jiraibqniwneibnab
 
 # TODO-1 : Create a function called decrypt() that takes original text and shift the amount of inputs
 def decrypt(original_text, shift_amount):
@@ -40,23 +39,23 @@ def decrypt(original_text, shift_amount):
         letter_index = alphabet.index(letter) - shift_amount
         letter_index %= len(alphabet)
         decrypted_message += alphabet[letter_index]
-
-    print(decrypted_message)
-
-
-decrypt(text, shift)
+    print(f"here is the encrypted message {decrypted_message}")
 
 
-# TODO-2 : inside the decrypt function shift each letter of the "original text forward in the alphabeth
-# and backwards the shift amount
-# TODO-3 combine the encryp and decrypt funcions into a single function called caesar
-# Use the value of the user chosen fuunctin direction variable to determine which functionality to use
-# call thecaesar function isntead of the encrypt/Decrypt and pass all 3 variables.
+def caesar(original_text, shift_amount):
+    if direction == 'encode':
+        encrypt(original_text, shift_amount)
+    elif direction == 'decode':
+        decrypt(original_text, shift_amount)
 
-# def caesar(original_text, shift, direction)
-#
-# if direction == 'encrypt':
-#     encrypt(text, shift)
-# elif direction == 'decrypt':
-#     decrypt(text, shift)
+
+while quit != "Y":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    if direction not in ("decode", "encode"):
+        print("Please type 'Decode' or 'Encode'")
+        continue
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number: \n"))
+    caesar(text, shift)
+    quit = input("Do you want to quit (Y) for Yes, (N) for No\n")
 
