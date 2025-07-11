ace76c8386a25d48d35a0ea4e4ae5b645595653a
LuigiLasso <llasso05@gmail.com>
2025-07-11 11:49:18 -0500

    Day 10 completed.

--- Day10/Calculator.py
+++ Day10/Calculator.py
@@ -1,3 +1,26 @@
+print("""
+           _            _       _             
+          | |          | |     | |            
+  ___ __ _| | ___ _   _| | __ _| |_ ___  _ __ 
+ / __/ _` | |/ __| | | | |/ _` | __/ _ \| '__|
+| (_| (_| | | (__| |_| | | (_| | || (_) | |   
+ \___\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|   
+    
+  ___                            ___
+ ||  |   ___         ___        ||  |
+ || _|__/  _\_______/  _\_______|| _|
+ ||(___(  (________(  (_________||((_)
+ ||  |  \___/       \___/       ||  |
+ ||  |         ___              ||  |
+ || _|________/  _\_____________|| _|
+ ||(_________(  (_______________||((_)
+ ||  |        \___/             ||  |
+ ||  |                          ||  |
+ ||  |                          ||  |
+ ||  |                          ||  |
+                            """
+)
+
 def add(n1, n2):
     return n1 + n2
 
@@ -22,8 +45,13 @@ operations = {
 }
 
 first_number = int(input("Please input your number here: "))
-operation = input("Please select your opeartion: '*', '/', '+','-' ")
-second_number = int(input("Please select your second number: "))
+another_calculation = "yes"
 
-calculation = operations[operation](first_number, second_number)
-print(calculation)
+while another_calculation == "yes":
+    for symbol in operations:
+        print(symbol)
+    operation = input("Please select your opeartion: ")
+    second_number = int(input("Please select your second number: "))
+    first_number = operations[operation](first_number, second_number)
+    print(first_number)
+    another_calculation = input("Do you want to do another calculation: (Yes/No) ").lower()
